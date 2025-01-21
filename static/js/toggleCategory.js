5fa35240faf2bcc52801b66e4818b228ed73292d
athooh <s.athooh@gmail.com>
2025-01-21 20:06:18 +0300

    update: implement freezing category dropdown unless user clicks out of box

--- static/js/toggleCategory.js
+++ static/js/toggleCategory.js
@@ -1,12 +1,13 @@
- // JavaScript to toggle the dropdown
- function toggleDropdown() {
+// JavaScript to toggle the dropdown
+function toggleDropdown() {
     const dropdownContent = document.getElementById("category-dropdown");
     dropdownContent.classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-toggle')) {
+    // Check if the click is outside both the dropdown toggle button and the dropdown content
+    if (!event.target.closest('.dropdown')) {
         const dropdowns = document.getElementsByClassName("dropdown-content");
         for (let i = 0; i < dropdowns.length; i++) {
             const openDropdown = dropdowns[i];
